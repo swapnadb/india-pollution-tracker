9904537f2daa32221f48a74580e2d9669df35eee
Bose <kbose@gmail.com>
2025-05-17 19:56:16 +0530

    github details updated

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -62,7 +62,7 @@ const About: React.FC = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
                   </svg>
-                  <a href="https://github.com/swapnaxdata" target="_blank" rel="noopener noreferrer" className="text-teal-600 hover:underline">github.com/swapnaxdata</a>
+                  <a href="https://github.com/swapnadb" target="_blank" rel="noopener noreferrer" className="text-teal-600 hover:underline">github.com/swapnadb</a>
                 </div>
                 
                 <div className="flex items-center">
